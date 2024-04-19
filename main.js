ce208278ad52fcd885e1d67ae3a4a9796b31dbb3
vivianesanta <108529110+vivianesanta@users.noreply.github.com>
2024-04-19 07:54:27 -0300

    aula 5

--- main.js
+++ main.js
@@ -18,4 +18,19 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+
+}
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+
