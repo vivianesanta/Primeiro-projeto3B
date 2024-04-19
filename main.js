2c23f8a55db429b75b169e7004c09a017ab84b9d
vivianesanta <108529110+vivianesanta@users.noreply.github.com>
2024-04-19 08:37:30 -0300

    aula 10

--- main.js
+++ main.js
@@ -41,11 +41,13 @@ function calculaTempo(tempoObjetivo) {
     }
 }
 
-function atualizaCronometro(){
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
-    }
+for (let i=0; i<contadores.length;i++){
+    document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+    document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+    document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+    document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
 }
+
 
 function comecaCronometro(){
     atualizaCronometro();
